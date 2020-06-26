6c528dba781aa4c6908d0b2a9fcaccc0ae4f30b5
Nhất Cao <an.dev2504@gmail.com>
2020-06-26 20:45:52 +0700

    cau truc thu muc + boc tach html sang jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./shared/components/Layout/Header";
+import Footer from "./shared/components/Layout/Footer";
+import Menu from "./shared/components/Layout/Menu";
+import Slider from "./shared/components/Layout/Slider";
+import SideBar from "./shared/components/Layout/SideBar";
+
+import HomePage from "./pages/Home";
+import ProductDetailPage from "./pages/ProductDetail";
+import CategoryPage from "./pages/Category";
+import SearchPage from "./pages/Search";
+import CartPage from "./pages/Cart";
+import NotFoundPage from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div id="body">
+        <div className="container">
+          <Menu />
+          <div class="row">
+            <div id="main" class="col-lg-8 col-md-12 col-sm-12">
+              <Slider />
+              <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route
+                  path="/product-detail-:id"
+                  exact
+                  component={ProductDetailPage}
+                />
+                <Route path="/category-:id" component={CategoryPage} exact />
+                <Route path="/search" exact component={SearchPage} />
+                <Route path="/cart" exact component={CartPage} />
+                <Route path="/404" exact component={NotFoundPage} />
+                <Route render={() => <Redirect to="/404" />} />
+              </Switch>
+            </div>
+            <SideBar />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
